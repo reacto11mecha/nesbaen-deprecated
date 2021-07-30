6d3eb45e824f967f02e441ce3c82b374e730393e
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-07-30 17:14:50 +0700

    Menambahkan css dan card dengan ukuran 60%

--- client/src/App/Components/LoginComponent.jsx
+++ client/src/App/Components/LoginComponent.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 
+import styles from "./Styles/LoginComponent.module.css";
+
 export default function LoginComponent() {
-  return <div className="container"></div>;
+  return (
+    <div className={`container ${styles.entireContainer}`}>
+      <div className={`row justify-content-center ${styles.formRow}`}>
+        <div className="col">
+          <div className="card">
+            <div className="card-body"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
