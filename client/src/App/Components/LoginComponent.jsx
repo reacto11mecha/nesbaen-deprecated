c19fc8519333729f18b7d53d204068fb8817e8a5
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-08-09 06:29:03 +0700

    Menambahkan form untuk login

--- client/src/App/Components/LoginComponent.jsx
+++ client/src/App/Components/LoginComponent.jsx
@@ -1,17 +1,64 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useForm } from "react-hook-form";
+import { Container, Row, Col, Card, Button, Form } from "react-bootstrap";
 
+import { UserContext } from "../Context/User";
 import styles from "./Styles/LoginComponent.module.css";
 
+const ErrorDanger = ({ message }) => (
+  <small className="text-danger">{message}</small>
+);
+
 export default function LoginComponent() {
+  const [, { submitLogin }] = useContext(UserContext);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm();
+
   return (
-    <div className={`container ${styles.entireContainer}`}>
-      <div className={`row justify-content-center ${styles.formRow}`}>
-        <div className="col">
-          <div className="card">
-            <div className="card-body"></div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Container>
+      <Row className={`justify-content-center ${styles.verticalCenter}`}>
+        <Col lg={8} md={12}>
+          <Card>
+            <Card.Body>
+              <Form onSubmit={handleSubmit(submitLogin)}>
+                <Form.Group className="mb-3">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control
+                    type="email"
+                    placeholder="Enter email"
+                    {...register("email", {
+                      required: true,
+                    })}
+                    readOnly={isSubmitting}
+                  />
+                  {errors.email?.type === "required" && (
+                    <ErrorDanger message="Email is required" />
+                  )}
+                </Form.Group>
+
+                <Form.Group>
+                  <Form.Label>Masukan kata sandi</Form.Label>
+                  <Form.Control
+                    type="password"
+                    placeholder="Password"
+                    {...register("password", { required: true })}
+                    readOnly={isSubmitting}
+                  />
+                  {errors.password?.type === "required" && (
+                    <ErrorDanger message="Password is required" />
+                  )}
+                </Form.Group>
+                <Button variant="primary" type="submit" className="mt-5">
+                  Submit
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 }
