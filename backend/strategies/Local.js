1d31d37260b131c0aa8b5087ef245debe9ef811a
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-08-14 11:05:12 +0700

    Menghapus passport, util, dan strategi passport serta menambahkan bcrypt

--- backend/strategies/Local.js
+++
@@ -1,6 +0,0 @@
-const passport = require("passport");
-const User = require("../models/user");
-const { Strategy: LocalStrategy } = require("passport-local");
-
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
